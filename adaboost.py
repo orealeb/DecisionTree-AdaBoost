1697b34f6ad7fcef14decc959b29094c2bbcd0be
Oreoluwa Alebiosu <orealeb@yahoo.com>
2014-12-02 23:15:06 -0600

    Update adaboost.py

--- adaboost.py
+++ adaboost.py
@@ -1,4 +1,5 @@
-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import scipy.io as sio
 from scipy import stats
@@ -7,133 +8,201 @@ import math
 import random
 import decisiontree
 import csv
-
-# original author https://github.com/varunkp/SpamDecisionTree
-
-train_filename = "./Datasets/sampled_training_Ore.csv"
-test_filename = "test.csv"
-training_data = decisiontree.Dataset(train_filename)
-test_data = decisiontree.Dataset(test_filename, True)
-target = "IsBadBuy"
+from numpy.random import *
 
 
-array = np.array
-
-#read in label
-labels = []
-for line in open("labels.csv"):
-    line = line.replace('"', '').strip()
-    labels.append(line)
-
-
-def adaboost(training_data, rounds):
-    m = training_data.leng
-    weights = np.ones(m) * 1.0 / m
-    strong_hypothesis = np.zeros(m)
-    learners = []
+def train(
+    training_data,
+    attr_names,
+    label_name,
+    num_attr,
+    num_rounds,
+    ):
+    num_rows = len(training_data)
+    classifiers = []
     alphas = []
+    weights = np.ones(num_rows) * 1.0 / num_rows
+    for n in range(num_rounds):
+        error = 0.
+        random_indices = resample(weights)
+        resampled_entries = []
 
+        for i in range(num_rows):
+            resampled_entries.append(training_data[random_indices[i]])
 
-    for t in range(rounds):
+        default = decisiontree.mode(resampled_entries, -1)
+        print 'round ' + str(n + 1) + ' training...'
+        weak_classifier = decisiontree.learn_decision_tree(
+            resampled_entries,
+            attr_names,
+            default,
+            label_name,
+            0,
+            num_attr,
+            )
 
-        error = 0.0
-        resampled_examples = []
-        examples_index = resample(weights, m)
-
-        for i in range(m):
-            resampled_examples.append(training_data.instances[examples_index[i]])
-
-
-        default = decisiontree.mode(resampled_examples, -1)
-        print  "round " + str(t+1) + " training..."
-        weak_learner = decisiontree.learn_decision_tree(resampled_examples, training_data.attr_names, default, target, 0) #id3.id3_depth_limited(resampled_examples, attributes, 2)
-        learners.append(weak_learner)
-
-        classifications = decisiontree.classify(training_data.instances, weak_learner)
+        classifications = decisiontree.classify(training_data,
+                weak_classifier)
         error = 0
         for i in range(len(classifications)):
             predicted = classifications[i]
-            error += (predicted != training_data.instances[i][-1])*weights[i]
-        
-        train_accuracy = decisiontree.tree_accuracy(resampled_examples, weak_learner)
-        print "Training Accuracy with sampled dataset= " + str(train_accuracy) + "%"
-         
-        train_accuracy = decisiontree.tree_accuracy(training_data.instances, weak_learner)
-        print "Training Accuracy with training dataset= " + str(train_accuracy) + "%"
+            error += (predicted != training_data[i][-1]) * weights[i]
 
-        print "Error", error
+        print 'Error', error
 
-        if error == 0.0:
+        if error == 0.:
             alpha = 4.0
         elif error > 0.5:
-            print    "Discarding learner"
-            continue    #discard classifier with error > 0.5
+            print 'Discarding learner'
+            continue  # discard classifier with error > 0.5
         else:
-            alpha = 0.5 * np.log((1 - error)/error)
+            alpha = 0.5 * np.log((1 - error) / error)
 
         alphas.append(alpha)
-        learners.append(weak_learner)
-        print  "weak learner added"
+        classifiers.append(weak_classifier)
+        print 'weak learner added'
 
-        for i in range(m):
-            y = training_data.instances[i][-1]
+        for i in range(num_rows):
+            y = training_data[i][-1]
             h = classifications[i]
-            h = -1 if h == 0 else 1
-            y = -1 if y == 0 else 1
+            h = (-1 if h == 0 else 1)
+            y = (-1 if y == 0 else 1)
             weights[i] = weights[i] * np.exp(-alpha * h * y)
         sum_weights = sum(weights)
         print 'Sum of weights', sum_weights
-        normalized_weights = [float(w)/sum_weights for w in weights]
+        normalized_weights = [float(w) / sum_weights for w in weights]
         weights = normalized_weights
-    
     return zip(alphas, learners)
 
 
-def classify(strong_hypothesis, example):
+def classify(weight_classifier, example):
     classification = 0
-    for weight, learner in strong_hypothesis:
-        if str(learner.predict(example)) == str(1):
-            ex_class = 1  
+    for (weight, classifier) in weight_classifier:
+        if str(classifier.predict(example)) == str(1):
+            ex_class = 1
         else:
             ex_class = -1
-        classification += weight*ex_class
-        #print str(learner.predict(example)) +" "+ str(classification) + " "+str(weight) + " "+ str(ex_class)
-
-    return 1 if classification > 0 else 0
+        classification += weight * ex_class
+    return (1 if classification > 0 else 0)
 
 
-
-def resample(weights, m):
-    xk = np.arange(m)
-    pk = weights
-    custm = stats.rv_discrete(name='custm', values=(xk, pk))
-    R = custm.rvs(size=m)
-    return R
-
-
-
-ret = adaboost(training_data, 5)
-print "adaboost building complete!"
-
-print ret
-
-predictions = []
-
-for row in test_data.instances:
-    predictions.append(classify(ret, row))
-
-i = 1
-for weight, learner in ret:
-    train_accuracy = decisiontree.tree_accuracy(training_data.instances, learner)
-    print "Training Accuracy for learner " + str(i) + ", weight " + str(weight) + " is " + str(train_accuracy) + "%"
-    i+=1
-
-res_file = open('adaboost_res.csv', 'w')
-writer = csv.writer(res_file)
-header = 'RefId','IsBadBuy'
-writer.writerow(header)  
-for i in range(len(predictions)):
-    writer.writerow([labels[i], predictions[i]])
-res_file.close()
+def resample(weights):
+    n = len(weights)
+    rand_indices = []
+    C = [0.] + [sum(weights[:i + 1]) for i in range(n)]
+    (u0, j) = (random(), 0)
+    for u in [(u0 + i) / n for i in range(n)]:
+        while u > C[j]:
+            j += 1
+        rand_indices.append(j - 1)
+    return rand_indices
 
 
+def main():
+    train_filename = './Datasets/66_SAMPLED.csv'
+    test_filename = './Datasets/66_test.csv'
+
+    # specify numeric attributes in input file
+
+    num_attr = [
+        True,
+        True,
+        True,
+        True,
+        True,
+        True,
+        True,
+        True,
+        True,
+        True,
+        True,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        ]
+    (attr_names, training_data) = \
+        decisiontree.read_data(train_filename, num_attr)
+    (attr_names, test_data) = decisiontree.read_data(test_filename,
+            num_attr)
+    label_name = 'IsBadBuy'
+
+    # read in ids
+
+    ids = []
+    for line in open('./Datasets/ids.csv'):
+        line = line.replace('"', '').strip()
+        ids.append(line)
+
+    ret = train(training_data, attr_names, label_name, num_attr, 5)
+    print 'adaboost building complete!'
+
+    print ret
+
+    predictions = []
+
+    for row in test_data:
+        predictions.append(classify(ret, row))
+
+    res_file = open('adaboost_res.csv', 'w')
+    writer = csv.writer(res_file)
+    header = ('RefId', 'IsBadBuy')
+    writer.writerow(header)
+    for i in range(len(predictions)):
+        writer.writerow([ids[i], predictions[i]])
+    res_file.close()
+
+
+if __name__ == '__main__':
+    main()
